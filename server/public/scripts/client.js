9d9a9b50b18d21356ee10ebbcdbff1c8bec7d89c
Astrid Pulse <astridcpulse@gmail.com>
2022-10-08 15:07:05 -0500

    set up on click function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,24 @@
-console.log('we\'re here for ya');
+const { response } = require("express");
+
+console.log('we\'re here for ya'); //test
 
 $(document).ready(onReady);
 
 function onReady(){
-    console.log('on READY');
+    console.log('on READY'); //test
+    $('#submitBtn').on('click', submitTask);
+}
+
+// submit new task on button click to data base function
+function submitTask(){
+    console.log('click function working'); //test
+
+    let newTask ={
+        name: $('#taskIn').val(),
+        notes: $('#notesIn').val(),
+        urge: $('#urgentIn').val(),
+        comp: $('#completeIn').val()
+    }
+    postNewTask(newTask);
 }
 
